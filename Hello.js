09d17b0f2caa906c8b6d86e6038f0806eb1e6734
vmeledath <vyasdice@gmail.com>
2019-06-22 20:43:14 +0530

    2 files modified

--- Hello.js
+++ Hello.js
@@ -1,3 +1,30 @@
 import React from 'react';
 
-export default ({ name }) => <h1>Hello {name}!</h1>;
+// let Hello = ( props ) => {  
+//   return <h1>{props.message} - {props.age}</h1>
+// }
+
+class Hello extends React.Component {
+  
+  constructor() {
+    console.log('constructor');
+    this.state = { message: '' };
+  }
+
+  render() {   
+     console.log('render');
+    return <p>{this.state.message}</p>
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    setTimeout(()=> this.setState({message:'hello123'}), 10000);
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
+}
+
+export default Hello;
